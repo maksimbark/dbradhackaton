8dc82e319f520296e8cb420d9ed6359a83d27c12
Maria Shipkova <maryshipkova@gmail.com>
2025-10-16 16:00:37 +0200

    init pages

--- src/pages/map/Map.tsx
+++ src/pages/map/Map.tsx
@@ -1,5 +1,6 @@
 import {useState} from 'react'
 // import './App.css'
+import './map.css'
 import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet'
 import '../../../node_modules/leaflet/dist/leaflet.css'
 
@@ -9,16 +10,13 @@ function App() {
     const [count, setCount] = useState(0)
 
     return (
-        <>
-            <h1>Map super page!</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    meow is {count} maps
-                </button>
-            </div>
-            <br/>
-            <div>
-                <MapContainer center={[51.505, 0]} zoom={13} scrollWheelZoom={false} style={{height: '300px'}}>
+        <div className="map-page">
+            <header className="map-header">
+                <h1>Map super page!</h1>
+            </header>
+
+            <main className="map-main">
+                <MapContainer center={[51.505, 0]} zoom={13} scrollWheelZoom={false} style={{height: '100%', width: '100%'}}>
                     <TileLayer
                         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -29,8 +27,16 @@ function App() {
                         </Popup>
                     </Marker>
                 </MapContainer>
-            </div>
-        </>
+            </main>
+
+            <footer className="map-footer">
+                <div className="card">
+                    <button onClick={() => setCount((count) => count + 1)}>
+                        meow is {count} maps
+                    </button>
+                </div>
+            </footer>
+        </div>
     )
 }
 
