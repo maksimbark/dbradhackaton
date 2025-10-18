d52379b4e1858fa196678e088ed634eef90c0118
Maria Shipkova <maryshipkova@gmail.com>
2025-10-18 12:32:44 +0200

    fix import

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import {BrowserRouter, Routes, Route} from "react-router";
 import './index.css'
-import App from './App.tsx'
+// import App from './App.tsx'
 import Map from './pages/map/Map.tsx'
 import Dbrad from './pages/dbrad/Dbrad.tsx'
 import DbradMap from './pages/dbrad-map/DbradMap.tsx'
